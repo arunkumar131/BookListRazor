30dba8f677e222050001ec0c228b6522e1882368
Arun Kumar <arun.kumar@klm.com>
2020-02-24 15:50:08 +0100

    Add Book Index Razor file

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookListRazor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
